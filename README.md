c2a4e937230ff875d74a067d3d81bcf6749b5b91
ShuhratBek <shuhratbek.26@gmail.com>
2016-01-14 16:09:51 +0300

    update

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
 # angular-training-task1
 Task1 for Training Angular (EPAM)
+
+##Task 1:
+ 
+Project requirements:
+- <firstname_lastname>
+                - <main_file>.js
+                - index.html
+                - package.json
+In the case of specific configuration of your project - provide list of commands with comments.
+======================================
+ 
+- download and install nodejs 4x version
+- configure package.json
+- install angularjs framework 1.4.*
+- create main module for the application with html template (simple module ng-app)
+- html should contain:
+                input element with name of user (one or two words)
+                input element with age number
+                input element with date format (YYYY/MM/DD | DD MMMM YYYY | DD.MM.YYYY | DD-MMM-YY)
+                greeting text with input text
+- html should have a form with validation
+- html should have a place with submitted information
+- create your own validators:
+ 
+string validator - more than 3 symbols, only first letter in uppercase of the word, only letters
+integer validator - age should be between 18 and 65
+
+##Requirements
+- ([AngularJS](https://angularjs.org/))
+- ([Angular Material](https://material.angularjs.org))
+- ([Moment.js](http://momentjs.com/))
+- ([Material Design Icont Font](https://design.google.com/icons/))
+
+##Install
+```bash
+$ npm install
+```
+
+##Information
+Used [JohnPapa Style Guide](https://github.com/johnpapa/angular-styleguide) for AngularJS
--- index.js
+++ index.js
@@ -1,57 +1,68 @@
-'use strict';
+(function() {
+	'use strict';
 
-angular
-	.module('task1App', [
-		'ngAnimate',
-    	'ngAria',
-		'ngSanitize', 
-		'ngMessages', 
-		'ngMaterial'
-	])
-	.config(function($mdThemingProvider) {
-		$mdThemingProvider
-			.theme('default')
-			.primaryPalette('teal');
-	})
-	.controller("MainCtrl", MainCtrl)
-	.directive('age', function() {
-		return {
-			require: 'ngModel',
-			link: function(scope, elm, attrs, ctrl) {
-				ctrl.$validators.incorrect = function(modelValue, viewValue) {
-			        return ctrl.$isEmpty(modelValue) || /[0-9]/.test(viewValue);
-			    };
-				ctrl.$validators.adult = function(modelValue, viewValue) {
-			        return (viewValue >= 18 && viewValue <= 65);
-			    };
-			}
+	angular
+		.module('task1App', [
+			'ngAnimate', 
+			'ngAria', 
+			'ngSanitize', 
+			'ngMessages', 
+			'ngMaterial'
+		])
+		.config(function($mdThemingProvider) {
+			// Theme configuration for angular-material
+			$mdThemingProvider
+				.theme('default')
+				.primaryPalette('teal');
+		})
+		.controller("MainCtrl", MainCtrl)
+		.directive('age', function() {
+			return {
+				require: 'ngModel',
+				link: function(scope, elm, attrs, ctrl) {
+					// validate incorrect
+					ctrl.$validators.incorrect = function(modelValue, viewValue) {
+				        return ctrl.$isEmpty(viewValue) || /[0-9]/.test(viewValue);
+				    };
+
+				    //validate adult
+					ctrl.$validators.adult = function(modelValue, viewValue) {
+				        return (viewValue >= 18 && viewValue <= 65);
+				    };
+				}
+			};
+		})
+		.directive('birthday', function($window) {
+			return {
+				require: 'ngModel',
+				link: function(scope, elm, attrs, ctrl) {
+					// Connect to Moment.js				
+	                var moment = $window.moment;
+
+	                // Check date is valid
+	                function isDate(value) {
+	                    var m = moment(value, 'DD MMMM YYYY', true);
+	                    return m.isValid();
+	                };
+
+	                // validate
+					ctrl.$validators.incorrect = function(modelValue, viewValue) {
+				        return isDate(viewValue);
+				    };
+				}
+			};
+		});
+
+	function MainCtrl(){
+		var vm = this;
+		// The controllerAs syntax uses this inside controllers which gets bound to $scope
+		// https://github.com/johnpapa/angular-styleguide#style-y031
+
+		vm.result = {};
+
+		// Submits form
+		vm.submit = function(){
+			angular.copy(vm.user, vm.result);
 		};
-	})
-	.directive('birthday', function($window) {
-		return {
-			require: 'ngModel',
-			link: function(scope, elm, attrs, ctrl) {				
-                var moment = $window.moment;
-                function isDate(value) {
-                    var m = moment(value, 'DD MMMM YYYY', true);
-                    return m.isValid();
-                };
-
-                ctrl.$parsers.push(function(value) {
-                    if (!value || value.length === 0) {
-                         return value;
-                    };
-                    ctrl.$setValidity('incorrect', isDate(value));
-                    return value;
-                });
-			}
-		};
-	});
-
-function MainCtrl(){
-	var vm = this;
-	vm.result = {};
-	vm.submit = function(){
-		angular.copy(vm.user, vm.result);
-	};
-}
+	}
+})();
